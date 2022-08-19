59649e1bdb3ae29763a94be1f78039ce9180cf47
Harish <87933016+unknownharish@users.noreply.github.com>
2022-08-19 14:50:00 +0530

    Update [id].jsx

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -176,7 +176,7 @@ export async function getServerSideProps(context) {
     let { params } = context;
     // console.log('in ssp ', params)
 
-    let res = await axios.get(`https://foodorder.vercel.app/api/product/${params.id}`)
+    let res = await axios.get(`https://myfoodorder.vercel.app/api/product/${params.id}`)
     // console.log(res);
 
 
